97818e03fdb9658964e6a3d480adbc8803a158d5
acozzi <acozzzi@gmail.com>
2020-03-05 16:05:52 +0000

    manage multiple signs & handle errors

--- include/script.js
+++ include/script.js
@@ -1,16 +1,69 @@
 
-function seleccion(id){
-	if(id==1){
-		document.getElementById("precio").innerHTML="Precio: 5.000";
-	} 
-	else if(id==2){
-		document.getElementById("precio").innerHTML="Precio: 20.000";
-	} 
-	else {
-		document.getElementById("precio").innerHTML="Precio: 40.000"
+var res=0;
+var calculo= new Array();
+calculo[0]=0;
+var contador=0;
+
+function escribir(numero){
+	if(document.getElementById("pantalla").innerHTML == '+'){
+		document.getElementById("pantalla").innerHTML=0;
+		document.getElementById("pantalla").innerHTML+=numero;
+	} else {
+		document.getElementById("pantalla").innerHTML+=numero;
 	}
 }
-function escribir(signo){
-	document.getElementById("pantalla").innerHTML+=signo;
+function resultado(){
+	//acumular();
+	res=calculo[0];
+	for(let i=1;i<calculo.length-1;i+=2){
+		res = operar(res, calculo[i+1],calculo[i]);	
+	}
+	document.getElementById("pantalla").innerHTML=res;
+	acumular();
+}
+function operar(valor1, valor2,operador){
+	switch(operador){
+		case '+':
+			return valor1+valor2;
+		break;
+		case '-':
+			return valor1-valor2;
+		break;
+		case '*':
+			return valor1*valor2;
+		break;
+		case '/':
+			return valor1/valor2;
+		break;
+	}
+}
+function signo(operador){
+	acumular(operador);
+	//contador++;
+	
+	/*
+	document.getElementById("pantalla").innerHTML=0;	
+	
+	if(typeof calculo[contador] == "number"){
+
+			
+	} else {
+		alert("ERROR: No se puede usar más de un operador por operación.");
+	}
+	*/
+}
+function borrar(){
+	calculo=new Array();
+	calculo[0]=0;
+	contador=0;
+	res=0;
+	document.getElementById("pantalla").innerHTML=0;
+}
+function acumular(operador){
+	calculo[contador]=document.getElementById("pantalla").innerHTML*1;
+	contador++;
+	document.getElementById("pantalla").innerHTML=operador;
+	calculo[contador]=operador;
+	contador++;
 }
 
